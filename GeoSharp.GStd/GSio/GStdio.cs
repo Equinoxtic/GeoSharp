a021de8b1fc9e87a74720ba759408e6ea1f19300
Equinoxtic <thisistherealequinoctic@gmail.com>
2023-03-31 22:46:57 +0800

    Update GStdio.cs
    Remove InteropServices

--- GeoSharp.GStd/GSio/GStdio.cs
+++ GeoSharp.GStd/GSio/GStdio.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 
 namespace GeoSharp.GStd.GSio {
 	public static class GStdio {
